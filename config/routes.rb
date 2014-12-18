4bedfad7aa52e3baf291bb686fe1054d45a5f72f
Chris Coles <cryan.coles@gmail.com>
2014-12-18 13:37:01 -0500

    Changes auth/facebook/setup route from a match to a GET

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback' => 'sessions#create'
   get 'signin' => 'sessions#new', :as => :signin
 
-  match '/auth/facebook/setup', :to => 'facebook#setup'
+  get '/auth/facebook/setup', :to => 'facebook#setup'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
