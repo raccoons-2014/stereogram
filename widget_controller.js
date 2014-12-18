4ca2880da6d757cdac03f76b33f914e2ee66e5a3
Alex Yanai & Patrick Torpey <pair+alexsyanai.torpeyp@devbootcamp.com>
2014-12-18 18:48:52 -0500

    JS is broken into snippet and widget model

--- widget_controller.js
+++ widget_controller.js
@@ -1,7 +1,13 @@
-var widgetContoller = function(start, stop) {
-  this.snippetTime = 0
-  widgetIframe = document.getElementById('sc-widget'),
-  widget = SC.Widget(widgetIframe);
+var widgetContoller = function(track) {
+  var start = track.startTime;
+  var stop = track.stopTime;
+  var id = track.trackID;
+
+  this.snippetTimes = []
+  $widgetIframe = $('#sc-widget');
+  trackString = "https://w.soundcloud.com/player/?url=http://api.soundcloud.com/tracks/" + track.trackID + "&buying=false&show_playcount=false&show_comments=false&sharing=false";
+  $widgetIframe.attr('src', trackString);
+  widget = SC.Widget($widgetIframe);
   widget.bind(SC.Widget.Events.READY, function() {
     widget.bind(SC.Widget.Events.PLAY, function() {
       widget.getCurrentSound(function(currentSound){});
@@ -19,9 +25,17 @@ var widgetContoller = function(start, stop) {
 widgetContoller.prototype.returnSnippet = function() {
   var that = this;
   widget.getPosition(function(pos){
-    that.snippetTime = pos;
+    that.snippetTimes.push(Math.floor(pos));
   })
 }
 
-test = new widgetContoller(0);
-Mousetrap.bindGlobal('command+j', function(){test.returnSnippet()})
+var someAudio = function(trackID, startTime, stopTime) {
+  this.trackID = trackID;
+  this.startTime = startTime;
+  this.endTime = stopTime;
+}
+
+song = new someAudio(177307209,30, 45);
+
+test = new widgetContoller(song);
+Mousetrap.bindGlobal('s n i p', function(){test.returnSnippet(), alert('SNIP!')})
