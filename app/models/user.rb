643b5fabe84d51c420cb94f2a7e2dcdb71367728
ZahidKarim <zahid.jethani@gmail.com>
2014-12-18 12:21:54 -0500

    created omniauth helper function in user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.uid = auth['uid']
+      if auth['info']
+        user.username = auth['info']['name'] ||  ""
+      end
+    end
+  end
 
 end
