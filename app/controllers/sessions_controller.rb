5e87c89851452d31026c18d553993bef36c90a12
ZahidKarim <zahid.jethani@gmail.com>
2014-12-18 18:46:05 -0500

    reroutes users to snippets page upon loggining in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     reset_session
     session[:user_id] = @user.id
 
-    redirect_to user_path(@user)
+    redirect_to snippets_path
   end
 
   def failure
