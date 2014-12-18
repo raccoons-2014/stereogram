82e51dfe74da6914cc7321825000f3db93f771f3
ZahidKarim <zahid.jethani@gmail.com>
2014-12-18 11:47:27 -0500

    creates create method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    auth = request.env["omniauth.auth"]
 
+    user = User.where(provider: auth["provider"],uid: auth["uid"].to_s).first || User.create_with_omniauth(auth)
+    reset_session
+    session[:user_id] = user.user_id
+    redirect_to users_create
   end
 
   def failure
