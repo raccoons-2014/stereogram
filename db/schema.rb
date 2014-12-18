d03477962ff5c5b0e3a914c255cad2fce435ff40
Chris Coles <cryan.coles@gmail.com>
2014-12-18 15:22:32 -0500

    Updates schema with correct snippet attribute, text

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141218192118) do
+ActiveRecord::Schema.define(version: 20141218195821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "snippets", force: true do |t|
+    t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
