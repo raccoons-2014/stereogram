448226729c348f4217c42d4e0cb75c9ba291475d
Liam McCartney <liam.mccartney@gmail.com>
2014-12-18 16:15:41 -0500

    update user migration and model with associations

--- db/migrate/20141218153525_create_users.rb
+++ db/migrate/20141218153525_create_users.rb
@@ -1,9 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :first_name
+      t.string :last_name
       t.string :provider
       t.string :uid
+      t.string :token
+      t.string :profile_img_url
 
       t.timestamps
     end
