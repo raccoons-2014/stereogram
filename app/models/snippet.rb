515d1ed494385540649610434875634051a09a1e
Liam McCartney <liam.mccartney@gmail.com>
2014-12-18 16:23:31 -0500

    set up snippet migration and model

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,2 +1,4 @@
 class Snippet < ActiveRecord::Base
+  belongs_to :track
+  belongs_to :user
 end
--- db/migrate/20141218195821_create_snippets.rb
+++ db/migrate/20141218195821_create_snippets.rb
@@ -1,7 +1,10 @@
 class CreateSnippets < ActiveRecord::Migration
   def change
     create_table :snippets do |t|
-      t.string :text
+      t.references :track
+      t.references :user
+      t.string :start_time
+      t.string :end_time
 
       t.timestamps
     end
