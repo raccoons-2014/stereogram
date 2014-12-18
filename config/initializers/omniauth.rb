38afbc1c1a2f887e5e9e05db58142288779e4770
ZahidKarim <zahid.jethani@gmail.com>
2014-12-18 11:35:13 -0500

    changed the config omni auth syntax

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['APP_ID'], ENV['APP_SECRET'],
-           # :scope => 'email,user_birthday,read_stream', :display => 'popup'
+  provider :facebook,Rails.application.secrets.omniauth_provider_key,Rails.application.secrets.omniauth_provider_secret,
+
 end
