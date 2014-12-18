d81bd2cb67e625e326bfb28626df8898b8ed7213
Chris Coles <cryan.coles@gmail.com>
2014-12-18 15:20:08 -0500

    Moves create action to snippet controller, successfully posts to FB

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,2 +1,15 @@
 class SnippetsController < ApplicationController
+
+  def create
+    @user = User.find(session[:user_id])
+    @snippet = Snippet.new(snippet_params)
+    @graph = Koala::Facebook::GraphAPI.new(@user.token)
+    @graph.put_wall_post(@snippet.text)
+    redirect_to :back
+  end
+
+  private
+    def snippet_params
+      params.require(:snippet).permit(:text)
+    end
 end
