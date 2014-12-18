84b93d7faa6e16f05bfcb2eb234c69aeec5c861d
Chris Coles <cryan.coles@gmail.com>
2014-12-18 18:04:03 -0500

    Adds track#new action

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,2 +1,16 @@
 class TracksController < ApplicationController
+  def new
+    @track = Track.new
+  end
+
+  def create
+    # if current_user
+      @track = user.tracks.create track_params
+
+  end
+
+  private
+    def track_params
+      params.require(:track).permit(:soundcloud_url, :user_id)
+    end
 end
