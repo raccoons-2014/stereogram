9f4a34463fdfbfd2066fb601f4fcf1347803a7f4
Liam McCartney <liam.mccartney@gmail.com>
2014-12-18 16:40:23 -0500

    define user with new attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,9 @@ class User < ActiveRecord::Base
       user.provider = auth['provider']
       user.uid = auth['uid']
       if auth['info']
-        user.username = auth['info']['name'] ||  ""
+        user.first_name = auth['info']['first_name'] ||  ""
+        user.last_name = auth['info']['last_name'] ||  ""
+        user.profile_img_url = auth['info']['image'] ||  ""
         user.token = auth['credentials']['token'] || ""
       end
     end
