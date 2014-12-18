db98e7901a5dff6d367244a307deaa736c7576bd
Chris Coles <cryan.coles@gmail.com>
2014-12-18 13:26:25 -0500

    Adds setup, true to omniauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Rails.application.secrets.omniauth_provider_key,
-                      Rails.application.secrets.omniauth_provider_secret
+                      Rails.application.secrets.omniauth_provider_secret,
+                      :setup => true
 
 end
