a99314ebc7b8eef28dc2834d299bc437613a0527
Alex Yanai & Patrick Torpey <pair+AlexSYanai.torpeyp@devbootcamp.com>
2014-12-19 12:05:32 -0500

    Allows snippets to load and autoplay without reload

--- widget_controller.js
+++ widget_controller.js
@@ -4,14 +4,12 @@ var widgetController = function(track) {
   var id = track.trackID;
   var that = this
   this.snippetTimes = [];
+  $('.widget').html('<iframe id="sc-widget" src="" width="100%" scrolling="no" frameborder="no"></iframe>')
   $widgetIframe = $('#sc-widget');
   this.trackString = "https://w.soundcloud.com/player/?url=http://api.soundcloud.com/tracks/" + track.trackID + "&buying=false&show_playcount=false&show_comments=false&sharing=false";
   $widgetIframe.attr('src', this.trackString);
   this.widget = SC.Widget($widgetIframe[0]);
 
-  //Emmit Ready Event... have another function that is listening for that event...
-
-  // play...
   this.widget.bind(SC.Widget.Events.READY, function() {
     that.widget.setVolume(50);
   });
@@ -34,25 +32,24 @@ widgetController.prototype.returnSnippet = function() {
   })
 }
 
-var someAudio = function(trackID, startTime, stopTime, val) {
+var audioTrack = function(trackID, startTime, stopTime, val) {
   this.trackID = trackID;
   this.startTime = startTime;
   this.stopTime = stopTime;
   this.$element = $('#'+ val);
 }
+
 songArray = [];
-song1 = new someAudio(177307209,30, 45, 1);
+song0 = new audioTrack(177307209,30, 45, 1);
+songArray.push(song0);
+song1 = new audioTrack(141551003,10, 50, 2);
 songArray.push(song1);
-song2 = new someAudio(141551003,10, 50, 2);
+song2 = new audioTrack(60173536,10, 50, 3);
 songArray.push(song2);
-song3 = new someAudio(60173536,10, 50, 3);
+song3 = new audioTrack(43432304,100, 115, 4);
 songArray.push(song3);
-song4 = new someAudio(43432304,100, 115, 4);
+song4 = new audioTrack(71965257);
 songArray.push(song4);
-song5 = new someAudio(71965257,10, 50, 5);
-songArray.push(song5);
-
-
 
 $.each(songArray, function(index, val) {
    $('.tracks').append("<img id='" + index +"' src='play.png'></>")
@@ -61,12 +58,11 @@ $.each(songArray, function(index, val) {
 $('.tracks').on('click', 'img', function(){
   var index = $(this).index();
   var clickedSong = songArray[index];
-  test = new widgetController(clickedSong);
-  test.widget.bind(SC.Widget.Events.READY,function(){
-    $widgetIframe.delay(300).css('display','block');
-    setTimeout(test.widget.play(),100);
+  currentSnippet = new widgetController(clickedSong);
+  currentSnippet.widget.bind(SC.Widget.Events.READY,function(){
+    currentSnippet.widget.play();
+    $widgetIframe.animate({opacity:1},1500,function(){});
   });
 });
 
-
-Mousetrap.bindGlobal('s n i p', function(){test.returnSnippet(), alert('SNIP!')})
+Mousetrap.bindGlobal('s n i p', function(){currentSnippet.returnSnippet(), alert('SNIP!')})
