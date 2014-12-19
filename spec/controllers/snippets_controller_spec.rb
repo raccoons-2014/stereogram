c9aa3a7dea1319f616b4e6b3b76aaaf8ededc2c4
Liam McCartney <liam.mccartney@gmail.com>
2014-12-18 19:00:24 -0500

    tests the create method of snippets controller

--- spec/controllers/snippets_controller_spec.rb
+++ spec/controllers/snippets_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 describe SnippetsController do
+  let (:test_user) {create :user}
+  let (:test_track) {create :track}
+  let (:test_snippet) {create :snippet}
 
+  before :each do
+    session[:user_id] = test_user.id
+  end
+
+  describe "#create" do
+    it "creates a snippet with valid params" do
+      expect{
+        post :create, track_id: test_track.id, snippet: attributes_for(:snippet)
+      }.to change {Snippet.count}.by(1)
+    end
+
+  end
 end
