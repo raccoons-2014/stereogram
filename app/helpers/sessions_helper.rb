cba28cb2ed29c00ac7955ed7e5054827f4954a5b
Liam McCartney <liam.mccartney@gmail.com>
2014-12-19 13:51:28 -0500

    cleans up sessionshelper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,20 +1,6 @@
 module SessionsHelper
-  def authenticate(user)
-    user.authenticate(params[:session][:password])
-  end
-
   def current_user
-    # if current user is nil then it will find the User and assign it to @current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-
-  def login(user)
-    session[:user_id] = user.id
-  end
-
-  def logout
-    session.delete(:user_id)
-    @current_user = nil
+    User.find_by(id: session[:user_id])
   end
 
   def redirect_guests
