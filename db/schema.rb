7002c68be6eb394b6a3b24a19fbb48002a4aaa9f
Chris Coles <cryan.coles@gmail.com>
2014-12-19 12:25:39 -0500

    Updates schema with correct column name, soundcluod_track_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141219031730) do
+ActiveRecord::Schema.define(version: 20141219163742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,10 @@ ActiveRecord::Schema.define(version: 20141219031730) do
   end
 
   create_table "tracks", force: true do |t|
-    t.string   "soundcloud_url"
-    t.integer  "user_id",        null: false
+    t.integer  "user_id",             null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "soundcloud_track_id"
   end
 
   create_table "users", force: true do |t|
