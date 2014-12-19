006e6c95d0f769cea8e941b779c455b83239d96f
Liam McCartney <liam.mccartney@gmail.com>
2014-12-18 22:55:46 -0500

    creates post route for signing in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def new
     session[:fb_permissions] = 'publish_actions'
     redirect_to '/auth/facebook'
@@ -6,6 +7,7 @@ class SessionsController < ApplicationController
 
   def create
     @auth = request.env["omniauth.auth"]
+
     @user = User.where(provider: @auth["provider"],uid: @auth["uid"].to_s).first || User.create_with_omniauth(@auth)
     reset_session
     session[:user_id] = @user.id
