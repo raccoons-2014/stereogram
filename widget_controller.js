9706c333f3e1b42146f0d24c591a305222a19522
Alex Yanai & Patrick Torpey <pair+alexsyanai.torpeyp@devbootcamp.com>
2014-12-18 21:24:33 -0500

    Add .READY delay to select

--- widget_controller.js
+++ widget_controller.js
@@ -1,16 +1,15 @@
-var widgetContoller = function(track) {
+var widgetController = function(track) {
   var start = track.startTime;
   var stop = track.stopTime;
   var id = track.trackID;
 
   this.snippetTimes = [];
   $widgetIframe = $('#sc-widget');
-  trackString = "https://w.soundcloud.com/player/?url=http://api.soundcloud.com/tracks/" + track.trackID + "&buying=false&show_playcount=false&show_comments=false&sharing=false";
-  $widgetIframe.attr('src', trackString);
+  this.trackString = "https://w.soundcloud.com/player/?url=http://api.soundcloud.com/tracks/" + track.trackID + "&buying=false&show_playcount=false&show_comments=false&sharing=false";
+  $widgetIframe.attr('src', this.trackString);
   widget = SC.Widget($widgetIframe[0]);
   widget.bind(SC.Widget.Events.READY, function() {
     widget.bind(SC.Widget.Events.PLAY, function() {
-      widget.getCurrentSound(function(currentSound){});
       widget.seekTo(start*1000);
       widget.bind(SC.Widget.Events.PLAY_PROGRESS, function() {
         widget.getPosition(function(position) {
@@ -22,7 +21,7 @@ var widgetContoller = function(track) {
   });
 };
 
-widgetContoller.prototype.returnSnippet = function() {
+widgetController.prototype.returnSnippet = function() {
   var that = this;
   widget.getPosition(function(pos){
     that.snippetTimes.push(Math.floor(pos));
@@ -35,12 +34,27 @@ var someAudio = function(trackID, startTime, stopTime, val) {
   this.stopTime = stopTime;
   this.$element = $('#'+ val);
 }
+songArray = [];
+song1 = new someAudio(177307209,30, 45, 1);
+songArray.push(song1);
+song2 = new someAudio(141551003,10, 50, 2);
+songArray.push(song2);
+song3 = new someAudio(60173536,10, 50, 3);
+songArray.push(song3);
+song4 = new someAudio(43432304,100, 115, 4);
+songArray.push(song4);
+song5 = new someAudio(71965257,10, 50, 5);
+songArray.push(song5);
+// test = new widgetController(song1);
 
-song = new someAudio(177307209,30, 45, 1);
-test = new widgetContoller(song);
-song.$element.on('click', function(){
-  $widgetIframe.css('display','block'),
-  widget.play()});
+$('body').on('click', 'img', function(){
+  var index = $(this).index();
+  var clickedSong = songArray[index];
+  test = new widgetController(clickedSong);
+  widget.bind(SC.Widget.Events.PLAY,function(){
+    $widgetIframe.css('display','block');
+  });
+});
 
 
 Mousetrap.bindGlobal('s n i p', function(){test.returnSnippet(), alert('SNIP!')})
