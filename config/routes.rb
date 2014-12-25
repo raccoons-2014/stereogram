de6b030a70ffd3174fadfd85069efd2c9f15dd36
Chris Coles <cryan.coles@gmail.com>
2014-12-25 18:43:22 -0500

    Removes unnecessary routes, as a result of Devise implementation

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,9 @@ Rails.application.routes.draw do
 
   post "tracks/upload", :as => :upload
 
-  get 'auth/:provider/callback' => 'sessions#create'
-  get 'signin' => 'sessions#new', :as => :signin
-  post 'signin' => 'sessions#create'
+  # get 'auth/:provider/callback' => 'sessions#create'
 
-  get '/auth/facebook/setup', :to => 'facebook#setup'
+  # get '/auth/facebook/setup', :to => 'facebook#setup'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
