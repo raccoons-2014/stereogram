1d08ceb7b11a22ea3dec054da627f17bc6796d2f
Chris Coles <cryan.coles@gmail.com>
2014-12-25 18:20:42 -0500

    Declares Users as omniauthable and explicitly lists omniauth_providers

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :omniauthable
+         :confirmable, :omniauthable, :omniauth_providers => [:facebook, :soundcloud]
   has_many :tracks
   has_many :snippets
 
