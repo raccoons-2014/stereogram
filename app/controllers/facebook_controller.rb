e019507c0eec0a58a08ef6ec68e6c3e9bb9934ff
Chris Coles <cryan.coles@gmail.com>
2014-12-26 18:05:22 -0500

    Removes methods from sessions controller and deletes facebook controller

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,6 +1,3 @@
 class FacebookController < ApplicationController
-  def setup
-    request.env['omniauth.strategy'].options[:scope] = session[:fb_permissions]
-    render :text => "Setup complete.", :status => 404
-  end
+
 end
