903fbb773c9bac5131bcd60d188eed2d379b4c87
Chris Coles <cryan.coles@gmail.com>
2014-12-26 16:48:48 -0500

    Moves create_by_omniauth method to omniauth_callback module

---
+++ app/controllers/concerns/omniauth_callbacks.rb
@@ -0,0 +1,50 @@
+module OmniauthCallbacks
+  extend ActiveSupport::Concern
+
+  def self.included(base)
+    base.class_eval do
+      has_many :omniauths, dependent: :destroy
+
+      def self.find_or_create_by_omniauth(auth)
+        unless user = User.find_by_omniauth(auth)
+          unless user = User.find_by_email(auth.info.email)
+            user = User.create_by_omniauth(auth)
+          end
+          user.bind_omniauth_service(auth)
+        end
+        user
+      end
+
+      def self.find_by_omniauth(auth)
+        includes(:omniauths).where(omniauths: {provider: auth.provider, uid: auth.uid}).first
+      end
+
+      def self.create_by_omniauth(auth)
+        User.create do |user|
+          user.provider = auth.provider
+          user.uid = auth.uid
+
+          if auth.info
+            user.first_name = auth.info.first_name
+            user.last_name = auth.info.last_name
+            user.email = auth.info.email
+            user.profile_img_url = auth.info.image
+            user.password = Devise.friendly_token[0,20]
+            user.confirmed_at = Time.now
+            user.token = auth.credentials.token
+          end
+        end
+      end
+
+      def bind_omniauth_service(auth)
+        Omniauth.create do |omniauth|
+          omniauth.user_id  = id
+          omniauth.provider = auth.provider
+          omniauth.uid      = auth.uid
+          omniauth.image    = auth.info.image
+          omniauth.url      = auth.info.urls.Facebook if auth.provider == 'facebook'
+        end
+      end
+    end
+  end
+end
