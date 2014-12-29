930ae2ee33f1b9d7cbeb78792f62a2a01ceb974c
Liam McCartney <liam.mccartney@gmail.com>
2014-12-29 17:15:34 -0500

    updates schema to reflect new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141219163742) do
+ActiveRecord::Schema.define(version: 20141229220845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20141219163742) do
     t.string   "profile_img_url", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",           null: false
+    t.string   "sound_cloud_id"
   end
 
 end
