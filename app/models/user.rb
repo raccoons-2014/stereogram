b9385342d4967042875e10c192c02bf1457134da
Liam McCartney <liam.mccartney@gmail.com>
2014-12-29 17:10:45 -0500

    adds email field to create_with_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
       if auth['info']
         user.first_name = auth['info']['first_name']
         user.last_name = auth['info']['last_name']
+        user.email = auth['info']['email']
         user.profile_img_url = auth['info']['image']
         user.token = auth['credentials']['token']
       end
