d8ec185324dfc696b69bd7334276f372f3ac68a0
Liam McCartney <liam.mccartney@gmail.com>
2014-12-29 17:38:38 -0500

    rewrites create_from_omniauth method so it finds or creates by

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,15 @@ class User < ActiveRecord::Base
   has_many :tracks
   has_many :snippets
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.first_name = auth['info']['first_name']
+      user.last_name = auth['info']['last_name']
+      user.email = auth['info']['email']
+      user.profile_img_url = auth['info']['image']
+      user.token = auth['credentials']['token']
+    end
+  end
 
   def self.create_with_omniauth(auth)
     create! do |user|
