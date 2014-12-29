a0a7f0c25096a9b5fd9f1a3fe269ed24d1ffa8bb
Liam McCartney <liam.mccartney@gmail.com>
2014-12-29 17:39:18 -0500

    removes old create_with omniauth method, adds new_with_session method

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,16 +15,10 @@ class User < ActiveRecord::Base
     end
   end
 
-  def self.create_with_omniauth(auth)
-    create! do |user|
-      user.provider = auth['provider']
-      user.uid = auth['uid']
-      if auth['info']
-        user.first_name = auth['info']['first_name']
-        user.last_name = auth['info']['last_name']
-        user.email = auth['info']['email']
-        user.profile_img_url = auth['info']['image']
-        user.token = auth['credentials']['token']
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
       end
     end
   end
