e4280871bfe5670d85134072774db2ea58e34ac3
Liam McCartney <liam.mccartney@gmail.com>
2014-12-29 23:31:48 -0500

    resets migration to include encrypted password

--- db/migrate/20141229220114_add_devise_to_users.rb
+++ db/migrate/20141229220114_add_devise_to_users.rb
@@ -1,5 +1,9 @@
 class AddDeviseToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :email, :string, null: false
+    change_table(:users) do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+    end
   end
 end
