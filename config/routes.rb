b9903c32f42f842895e383eec5d170cdfcf5a437
Liam McCartney <liam.mccartney@gmail.com>
2014-12-29 19:35:20 -0500

    customizes sign in and sign out routes with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, skip: [:sessions], :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   devise_scope :user do
-    get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+      get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+      post 'sign_in', :to => 'devise/session#create', :as => :user_session
+      get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
   root 'snippets#index'
