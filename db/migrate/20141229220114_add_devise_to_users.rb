a13efcc1a6e859934b64f1979085f42e0f6777ac
Liam McCartney <liam.mccartney@gmail.com>
2014-12-29 23:36:26 -0500

    removes default email value

--- db/migrate/20141229220114_add_devise_to_users.rb
+++ db/migrate/20141229220114_add_devise_to_users.rb
@@ -1,8 +1,7 @@
 class AddDeviseToUsers < ActiveRecord::Migration
   def change
     change_table(:users) do |t|
-      ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false
       t.string :encrypted_password, null: false, default: ""
     end
   end
