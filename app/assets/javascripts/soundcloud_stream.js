9d7bdbafc59635032c98776f64c867942e35869c
torpeyp <torpey.patj@gmail.com>
2014-12-30 15:52:52 -0500

    search JS controller

--- app/assets/javascripts/soundcloud_stream.js
+++ app/assets/javascripts/soundcloud_stream.js
@@ -2,20 +2,30 @@ SC.initialize({
   client_id: request.env['SOUNDCLOUD_APP_ID']
 });
 
-var songView = function(){
 
+
+var songView = function(){
+  $body = $('body');
+  $startButton = $('input'); // specify later
+  $stopButton = $('input');
+  $playbackWidget = $('widget');
 
 
 }
 
-var songLoad = function(){
-
-
-
+var songLoad = function(searchParam){
+  this.searchParam = searchParam
 }
 
 var snippetController = function(){
 }
 
-snippetController.prototype.getSong = function(track, artist) {
-  SC.get('/tracks', {q:track,q:artist}, function(tracks){});}
+songLoad.prototype.searchSongs = function(){
+    this.searchResults = [];
+    that = this;
+    SC.get('http://api.soundcloud.com/tracks', {q:that.searchParam, limit:5}, function(tracks){
+      $(tracks).each(function(i, track){
+        that.searchResults.push(track.id);
+      });
+    });
+}
