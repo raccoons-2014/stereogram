f6bbd990cba2ca5b253824cc60dd91b0c81f7c8f
Liam McCartney <liam.mccartney@gmail.com>
2014-12-30 12:50:00 -0500

    changes AWS key names in env to proper names

--- config/application.rb
+++ config/application.rb
@@ -28,8 +28,8 @@ module Stereogram
     # config.i18n.default_locale = :de
 
     AWS::S3::Base.establish_connection!(
-    :access_key_id     => ENV["AWS_KEY"],
-    :secret_access_key => ENV["AWS_SECRET"]
+    :access_key_id     => ENV["AWS_ACCESS_KEY_ID"],
+    :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
     )
 
   end
