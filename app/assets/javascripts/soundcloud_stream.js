d9bd145fea2afe1c04d4572099fcef0f971e1737
torpeyp <torpey.patj@gmail.com>
2014-12-30 17:55:16 -0500

    Creates Track model

--- app/assets/javascripts/soundcloud_stream.js
+++ app/assets/javascripts/soundcloud_stream.js
@@ -1,31 +1,43 @@
-SC.initialize({
-  client_id: request.env['SOUNDCLOUD_APP_ID']
-});
 
 
+var track = function(opts){
+  this.id = opts[id];
+  this.permalinkUrl = opts[permalinkUrl];
+  this.artworkUrl = opts[artworkUrl];
+  this.description = opts[description];
+  this.duration = opts[duration];
+  this.waveformUrl = opts[waveformUrl];
+  this.bpm = opts[bpm];
+}
 
 var songView = function(){
   $body = $('body');
-  $startButton = $('input'); // specify later
+  $startButton = $('input');
   $stopButton = $('input');
   $playbackWidget = $('widget');
-
-
 }
 
-var songLoad = function(searchParam){
-  this.searchParam = searchParam
+var songLoad = function(){
+  this.userTracks = [];
 }
 
 var snippetController = function(){
 }
 
 songLoad.prototype.searchSongs = function(){
-    this.searchResults = [];
-    that = this;
-    SC.get('http://api.soundcloud.com/tracks', {q:that.searchParam, limit:5}, function(tracks){
-      $(tracks).each(function(i, track){
-        that.searchResults.push(track.id);
-      });
+  that = this;
+  SC.get('http://api.soundcloud.com/me/tracks', function(tracks){
+    $(tracks).each(function(i, userTrack){
+      that.userTracks.push(new track(
+        opts = {
+          id = userTrack.id,
+          permalinkUrl = userTrack.permalinkUrl,
+          artworkUrl = userTrack.artworkUrl,
+          description = userTrack.description,
+          duration = userTrack.duration,
+          waveformUrl = userTrack.waveformUrl,
+          bpm = userTrack.bpm
+        }));
     });
+  });
 }
