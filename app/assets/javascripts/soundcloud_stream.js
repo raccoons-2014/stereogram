5ec143e08351a721f0b18be9a62ef893ee583e7e
torpeyp <torpey.patj@gmail.com>
2014-12-30 19:20:40 -0500

    Cleans up js

--- app/assets/javascripts/soundcloud_stream.js
+++ app/assets/javascripts/soundcloud_stream.js
@@ -1,13 +1,17 @@
-
+$('#connect').on('click', function(){
+  preventDefault();
+  SC.connect(function(){
+  });
+});
 
 var track = function(opts){
-  this.id = opts[id];
-  this.permalinkUrl = opts[permalinkUrl];
-  this.artworkUrl = opts[artworkUrl];
-  this.description = opts[description];
-  this.duration = opts[duration];
-  this.waveformUrl = opts[waveformUrl];
-  this.bpm = opts[bpm];
+  this.id = opts.id;
+  this.permalinkUrl = opts.permalinkUrl;
+  this.artworkUrl = opts.artworkUrl;
+  this.description = opts.description;
+  this.duration = opts.duration;
+  this.waveformUrl = opts.waveformUrl;
+  this.bpm = opts.bpm];
 }
 
 var songView = function(){
@@ -28,16 +32,17 @@ songLoad.prototype.searchSongs = function(){
   that = this;
   SC.get('http://api.soundcloud.com/me/tracks', function(tracks){
     $(tracks).each(function(i, userTrack){
-      that.userTracks.push(new track(
-        opts = {
-          id = userTrack.id,
-          permalinkUrl = userTrack.permalinkUrl,
-          artworkUrl = userTrack.artworkUrl,
-          description = userTrack.description,
-          duration = userTrack.duration,
-          waveformUrl = userTrack.waveformUrl,
-          bpm = userTrack.bpm
-        }));
+      var opts = {
+        id: userTrack.id,
+        permalinkUrl: userTrack.permalinkUrl,
+        artworkUrl: userTrack.artworkUrl,
+        description: userTrack.description,
+        duration: userTrack.duration,
+        waveformUrl: userTrack.waveformUrl,
+        bpm: userTrack.bpm
+      }
+      that.userTracks.push(new track(opts)
+      );
     });
   });
 }
