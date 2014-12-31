8bf94fc0ae14526d7669119ff470d24fec3089ac
Liam McCartney <liam.mccartney@gmail.com>
2014-12-31 12:33:38 -0500

    adds to the Omniauth mock up to create valid user

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,7 +62,8 @@ RSpec.configure do |config|
     :info          => {
       :first_name  => Faker::Name.first_name,
       :last_name   => Faker::Name.last_name,
-      :image       => Faker::Internet.url
+      :image       => Faker::Internet.url,
+      :email       => Faker::Internet.email
     },
     :credentials   => {
       :token => "nicetoken12443"
