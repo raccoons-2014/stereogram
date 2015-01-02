5a02b5395ff4df78b33763dabea761765ef34b8c
Alex Yanai <yanai.alex@gmail.com>
2015-01-02 12:18:03 -0500

    Adds Follows relationship table

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,13 @@ ActiveRecord::Schema.define(version: 20150103143325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "follows", force: true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "snippets", force: true do |t|
     t.integer  "track_id"
     t.integer  "user_id",    null: false
