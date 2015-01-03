0602b37f9127361420e845888a5436a254f758e7
Alex Yanai <yanai.alex@gmail.com>
2015-01-03 18:01:59 -0500

    adds test for Follow controller creation

--- spec/controllers/follows_controller_spec.rb
+++ spec/controllers/follows_controller_spec.rb
@@ -4,8 +4,12 @@ describe FollowsController do
   let (:test_user) {create :user}
   let (:test_follower) {create :user}
 
-  before :each do
-    sign_in :user, test_user
-  end
+  describe "#create" do
+    it "should add a user as a follower" do
+      expect(
+        post :create, follow: {followed_id: (test_user.follow(test_follower))}
+      ).to be_redirect
+    end
 
+  end
 end
