9fae24fc033fe33bf652c8ee574d14118c9a3f00
Liam McCartney <liam.mccartney@gmail.com>
2015-01-02 19:04:40 -0500

    sets creation from ajax call, allows more attributes in params

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -23,15 +23,22 @@ class TracksController < ApplicationController
   end
 
   def create
-
+    binding.pry
     if request.xhr?
-      params[:tracks].each do |track_data|
-        track = current_user.tracks.new(track_data)
+      params[:tracks].each do |key, track_data|
+        binding.pry
+        track = current_user.tracks.new(
+          source_id: track_data['source_id'],
+          permalink_url: track_data['permalink_url'],
+          artwork_url: track_data['artwork_url'],
+          description: track_data['description'],
+          duration: track_data['duration'],
+          waveform_url: track_data['waveform_url'],
+          bpm: track_data['bpm']
+          )
         next unless track.save
       end
-      redirect_to user_path(current_user)
-    else
-      @track = current_user.tracks.new(track_params)
+    redirect_to user_path(current_user)
 
     if @track.save
       redirect_to track_path @track
@@ -56,7 +63,7 @@ class TracksController < ApplicationController
 
   private
     def track_params
-      params.require(:track).permit(:soundcloud_track_id, :user_id)
+      params.require(:track).permit(:source_id, :permalink_url, :artwork_url, :description, :duration, :waveform_url, :bpm)
     end
 
     def sanitize_filename(file_name)
