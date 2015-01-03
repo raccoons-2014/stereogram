202395dc57a286555b5830cb2506d9a4ac7e0235
ZahidKarim <zahid.jethani@gmail.com>
2015-01-03 12:55:37 -0500

    Creates associations in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :votes, as: :votable
+  has_many :votes
   devise :database_authenticatable, :rememberable, :omniauthable, :omniauth_providers => [:facebook]
   has_many :tracks
   has_many :snippets
