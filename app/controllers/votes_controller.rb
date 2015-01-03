f12f640071dbd2854b7f1ad137a0fc443981de32
ZahidKarim <zahid.jethani@gmail.com>
2015-01-03 13:22:42 -0500

    Adds a create method in controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,20 @@
 class VotesController < ApplicationController
 
+  def create
+    user = current_user
+    @vote = user.votes.new
+
+    if params[:track_id]
+      @track = Track.find(params[:track_id])
+      @track.votes << @vote unless already_voted?(@track)
+      redirect_to :back
+    else
+      @snippet = Snippet.find(params[:snippet_id])
+      @snippet.votes << @vote unless already_voted?(@snippet)
+      redirect_to :back
+    end
+  end
+
 
   private
     def vote_params
