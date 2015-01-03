32f660bdd978518b99a623a78eceddb2475a881a
Alex Yanai <yanai.alex@gmail.com>
2015-01-03 11:59:07 -0500

    creates follows controller

---
+++ app/controllers/follows_controller.rb
@@ -0,0 +1,15 @@
+class FollowsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def create
+    @user = User.find(params[:follow][:followed_id])
+    current_user.follow!(@user)
+    redirect_to @user
+  end
+
+  def destroy
+    @user = Follow.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    redirect_to @user
+  end
+end
