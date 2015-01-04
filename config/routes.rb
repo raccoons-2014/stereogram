05874682421dd5890d2fb937b4ba8f5d56dc03f0
torpeyp <torpey.patj@gmail.com>
2015-01-04 18:31:17 -0500

    Deletes get tracks route

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,6 @@ Rails.application.routes.draw do
     get 'following','followers', on: :member
   end
 
-  get 'users/:id/userTracks', to: "users#getTracks"
-
   resources :users, only: [:show]
   resources :tracks, only: [:index, :new, :create, :show, :destroy]
   resources :snippets, only: [:index, :show, :create, :destroy]
