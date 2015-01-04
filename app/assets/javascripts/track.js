a58fc16232af794a773c2fcc06655c45725e2e19
torpeyp <torpey.patj@gmail.com>
2015-01-04 14:18:37 -0500

    Changes track requirements to have track id to source id

--- app/assets/javascripts/track.js
+++ app/assets/javascripts/track.js
@@ -1,5 +1,5 @@
 function Track(opts){
-    this.source_id = opts.id;
+    this.source_id = opts.source_id;
     this.permalink_url = opts.permalink_url;
     this.artwork_url = opts.artwork_url;
     this.description = opts.description;
@@ -13,3 +13,7 @@ function Track(opts){
 Track.prototype.getStreamUrl = function() {
   return "http://api.soundcloud.com/tracks/"+ this.source_id +"/stream?client_id=ce2d05f718bd31feba35c78daaa99c23"
 };
+
+Track.prototype.audioCreator = function(){
+    this.audio = new Audio(this.stream_url)
+}
