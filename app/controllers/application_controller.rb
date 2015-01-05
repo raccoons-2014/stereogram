8b5dddbf3fea690859cf413eb25dbb46954562e7
ZahidKarim <zahid.jethani@gmail.com>
2015-01-05 11:27:23 -0500

    Adds layout method to display layout false if logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+    layout :layout
+
+  private
+
+  def layout
+    # only turn it off for login pages:
+    if current_user == nil
+      "devise/sessions"
+    else
+      "application"
+    end
+  end
+
 end
