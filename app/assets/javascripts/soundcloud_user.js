59b9288e7c762b4562a8c1d8d23115f6b52521f7
torpeyp <torpey.patj@gmail.com>
2015-01-05 16:11:24 -0500

    Deletes JSON datatype in AJAX call

--- app/assets/javascripts/soundcloud_user.js
+++ app/assets/javascripts/soundcloud_user.js
@@ -4,17 +4,18 @@ var SoundcloudUser = function(){
 }
 
 SoundcloudUser.prototype.searchSongs = function(){
-  SC.connect(function(){
-    SC.get('/me/tracks', function(
-      foundTracks){
-      $.ajax({
-        url: '/tracks',
-        method: 'POST',
-        data: {tracks: foundTracks},
-        dataType: 'JSON'
-      }).success(function(response){
-        $('body').append(response)
-      })
+    SC.connect(function(){
+        SC.get('/me/tracks', function(
+          foundTracks){
+          $.ajax({
+            url: '/tracks',
+            method: 'POST',
+            data: {tracks: foundTracks},
+          }).success(function(response){
+            console.log(response)
+            $('body').append(response)
+          })
+        })
     })
   })
 }
