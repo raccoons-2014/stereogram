f757b4e4feb33a2bafecedf8166d5fd8bf8ff28b
ZahidKarim <zahid.jethani@gmail.com>
2015-01-06 16:24:41 -0500

    refactor

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,26 +1,3 @@
-// $('img.expand_left').on("click", function() {
-//     $('img.expand_left').hide()
-//     $('#right.small-5.columns').animate({
-//       left:"600px"},500);
-//     $('.profile_image').css(
-//       "padding-left","17em");
-//     $('.username').show;
-//     $('.follow_div').show;
-//     $('img.expand_right').show()
-// })
-
-// $('img.expand_right').on("click", function() {
-//     $(this).hide()
-//     $('#right.small-5.columns').animate({
-//       left:"1045px"},500);
-//     $('.profile_image').css(
-//       "padding-left","3em");
-//     $('.username').hide;
-//     $('.follow_div').hide;
-//     $('img.expand_left').show()
-// })
-
-
 $('.row.collapse.track.partial').mouseover(function() {
     $(this).css(
       "background-color","#E8B504");
