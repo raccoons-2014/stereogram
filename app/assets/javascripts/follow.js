fdd8e5f3038edfabb96ddd49413d67d264997301
Alex Yanai <yanai.alex@gmail.com>
2015-01-06 18:59:56 -0500

    changes appends to html in ajax

--- app/assets/javascripts/follow.js
+++ app/assets/javascripts/follow.js
@@ -1,12 +1,10 @@
 $('.small-block-grid-3').on('click', 'img.follow', function(event){
   var userId = event.currentTarget.dataset.userId
-  debugger
   $.ajax({
     url: '/follows',
     method: 'POST',
     data: {follow:userId}
   }).done(function(response){
-    debugger
-    $('.follow_div_1').append(response)
+    $('.follow_div_1').html(response)
   })
 })
