fa7f089b3abdb41c2cf13c86f6e03b72be004e5f
ZahidKarim <zahid.jethani@gmail.com>
2015-01-06 16:03:16 -0500

    Redesign of layout and partial

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,20 +1,24 @@
-$('#right.small-5.columns').mouseover(function() {
-    $('#right.small-5.columns').animate({
-      left:"600px"},500);
-    $('.profile_image').css(
-      "padding-left","17em");
-    $('.username').show;
-    $('.follow_div').show;
-})
+// $('img.expand_left').on("click", function() {
+//     $('img.expand_left').hide()
+//     $('#right.small-5.columns').animate({
+//       left:"600px"},500);
+//     $('.profile_image').css(
+//       "padding-left","17em");
+//     $('.username').show;
+//     $('.follow_div').show;
+//     $('img.expand_right').show()
+// })
 
-$('#right.small-5.columns').mouseleave(function() {
-    $('#right.small-5.columns').animate({
-      left:"1045px"},500);
-    $('.profile_image').css(
-      "padding-left","3em");
-    $('.username').hide;
-    $('.follow_div').hide;
-})
+// $('img.expand_right').on("click", function() {
+//     $(this).hide()
+//     $('#right.small-5.columns').animate({
+//       left:"1045px"},500);
+//     $('.profile_image').css(
+//       "padding-left","3em");
+//     $('.username').hide;
+//     $('.follow_div').hide;
+//     $('img.expand_left').show()
+// })
 
 
 $('.row.collapse.track.partial').mouseover(function() {
