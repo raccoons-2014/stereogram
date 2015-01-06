c5f478ced0a6a0ff5e971bbfe2ceef3f0a1d270b
ZahidKarim <zahid.jethani@gmail.com>
2015-01-06 06:45:32 -0500

    Shifts profile image when scrolling over div

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -3,10 +3,14 @@ $(document).ready(function(){
   $('#right.small-5.columns').mouseover(function() {
       $('#right.small-5.columns').animate({
         left:"140px"},500);
+      $('.profile_image').css(
+        "padding-left","17em");
   })
 
   $('#right.small-5.columns').mouseleave(function() {
       $('#right.small-5.columns').animate({
         left:"625px"},500);
+      $('.profile_image').css(
+        "padding-left","3em");
   })
 })
