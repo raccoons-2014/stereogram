da8d19b002276b6ab49eda6545295dea00664b8d
Liam McCartney <liam.mccartney@gmail.com>
2015-01-05 19:01:41 -0500

    makes soundclouduser call searchsongs on instantiation, fixes tabbing

--- app/assets/javascripts/soundcloud_user.js
+++ app/assets/javascripts/soundcloud_user.js
@@ -1,25 +1,20 @@
 var SoundcloudUser = function(){
-    this.userTracks = [],
-    this.userPerm = ""
+  this.userTracks = [],
+  this.searchSongs();
 }
 
 SoundcloudUser.prototype.searchSongs = function(){
-  SC.initialize({
-    client_id: "b99714ff12392f9d7e1498ab5166732d",
-    redirect_uri: 'http:localhost:3000/callback'
-  });
-
-    SC.connect(function(){
-        SC.get('/me/tracks', function(
-          foundTracks){
-          $.ajax({
-            url: '/tracks',
-            method: 'POST',
-            data: {tracks: foundTracks},
-            dataType: 'JSON'
-          }).success(function(response){
-            $('body').append(response)
-          })
-        })
+  SC.connect(function(){
+    SC.get('/me/tracks', function(
+      foundTracks){
+      $.ajax({
+        url: '/tracks',
+        method: 'POST',
+        data: {tracks: foundTracks},
+        dataType: 'JSON'
+      }).success(function(response){
+        $('body').append(response)
+      })
     })
-  }
+  })
+}
