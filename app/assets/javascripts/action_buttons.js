9da1d19d3df0426f31aea4b9fccff36a7f591c07
Chris Coles <cryan.coles@gmail.com>
2015-01-07 02:48:17 -0500

    Removes unnecessary comments

--- app/assets/javascripts/action_buttons.js
+++ app/assets/javascripts/action_buttons.js
@@ -9,23 +9,12 @@ Throbber.prototype.throb_on = function(){
 };
 
 Throbber.prototype.throb_off = function() {
+  var this = that
   setTimeout(function() {
-    this.$actionButton.toggleClass(this.$actionButtonClass);
+    that.$actionButton.toggleClass(this.$actionButtonClass);
   }, 2000);
 };
 
 heartThrobber = new Throbber($('<div class="heart-image"></div>'), 'heart pulse2');
 heartThrobber.throb_on();
 heartThrobber.throb_off();
-
-// $('.row.collapse.track.partial').on('click', function(event){
-//   var heartImage = $(this).find('.heart-image')
-//   throb(heartImage);
-// });
-
-// var throb = function(heart){
-//   heart.toggleClass('heart pulse2');
-//     setTimeout(function() {
-//   heart.toggleClass('heart pulse2');
-//   }, 2000);
-// }
