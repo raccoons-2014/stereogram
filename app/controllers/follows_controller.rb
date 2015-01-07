5f6ddb7ed3e15a1501b9650540b8857b73f02568
Alex Yanai <yanai.alex@gmail.com>
2015-01-06 19:00:59 -0500

    adds logic to prevent multiple follows

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,7 +3,7 @@ class FollowsController < ApplicationController
 
   def create
     @user = User.find(params[:follow])
-    current_user.follow(@user) unless current_user.id == @user.id
+    current_user.follow(@user) unless current_user.id == @user.id || current_user.is_following?(@user)
     render plain: @user.followers.count
   end
 
