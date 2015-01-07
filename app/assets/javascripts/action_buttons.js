99d58f3daa733c8117a7e26ec6784228b0d8fbd4
Chris Coles <cryan.coles@gmail.com>
2015-01-07 10:26:17 -0500

    No changes, pulling from master

--- app/assets/javascripts/action_buttons.js
+++ app/assets/javascripts/action_buttons.js
@@ -3,18 +3,23 @@ var Throbber = function(actionButton, actionButtonClass) {
   this.actionButtonClass = actionButtonClass;
 }
 
-Throbber.prototype.throb_on = function(){
+Throbber.prototype.throb = function(){
   this.$actionButton.toggleClass(this.$actionButtonClass);
   throb_off();
 };
 
 Throbber.prototype.throb_off = function() {
-  var this = that
   setTimeout(function() {
-    that.$actionButton.toggleClass(this.$actionButtonClass);
+    this.$actionButton.toggleClass(this.$actionButtonClass);
   }, 2000);
 };
 
 heartThrobber = new Throbber($('<div class="heart-image"></div>'), 'heart pulse2');
-heartThrobber.throb_on();
+heartThrobber.throb();
 heartThrobber.throb_off();
+
+// Throbber.prototype.throb = function() {
+//
+// }
+// implement callback function that throbs first, and then throbs off
+// as of now, callback is not being hit
