65e263ae3cd69f01d636465035774e436b2df314
ZahidKarim <zahid.jethani@gmail.com>
2015-01-07 12:09:01 -0500

    Fixes hover colors

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,9 +1,15 @@
 $('.row.collapse.track.partial').mouseover(function() {
     $(this).css(
       "background-color","#E8B504");
+    var span = $(this).find("span")
+    span.css(
+      "color","black");
 })
 
 $('.row.collapse.track.partial').mouseleave(function() {
     $(this).css(
       "background-color","black");
+    var span = $(this).find("span")
+      span.css(
+      "color","#E8B504")
 })
