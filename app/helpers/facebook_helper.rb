34d036241e59255f50d3592f1058cf1f9f89845c
Liam McCartney <liam.mccartney@gmail.com>
2015-01-06 23:10:51 -0500

    adds facebook helper module to allow sharing

---
+++ app/helpers/facebook_helper.rb
@@ -0,0 +1,9 @@
+module FacebookHelper
+  def share(args)
+    user = FbGraph::User.me(args[:token])
+    user.link!(
+      link: "http://cryptic-dusk-7305.herokuapp.com",
+      message: 'I just created a Snippet on Stereogram'
+    )
+  end
+end
