de755558959e2384245b1b688722b4358844d3fb
Liam McCartney <liam.mccartney@gmail.com>
2015-01-08 00:23:45 -0500

    refactors method to allow for tracks or snippets sharing

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -2,9 +2,10 @@ module FacebookHelper
 
   def share(args)
     user = FbGraph::User.me(args[:token])
+
     user.link!(
-      link: "http://stere-o-gram.herokuapp.com/snippets/#{args[:snippet_id]}}",
-      message: 'I just created a Snippet on Stereogram'
+      link: "http://stere-o-gram.herokuapp.com/#{args[:item].class.to_s.downcase}s/#{args[:item].id}}",
+      message: "Check out this #{args[:item].class.to_s} on Stereogram"
     )
   end
 end
