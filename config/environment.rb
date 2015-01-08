04619c8103bf0774c140ac9caae84f677a4ae78d
Liam McCartney <liam.mccartney@gmail.com>
2015-01-08 01:22:27 -0500

    rms conditional SC_Redirect ENV variable

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-if Rails.env.production?
-  ENV['SC_REDIRECT'] = 'http://stere-o-gram.herokuapp.com/callback'
-else
-  ENV['SC_REDIRECT'] = 'http://localhost:3000/callback'
-end
