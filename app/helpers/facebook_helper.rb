6d1611c835ce7093952af501449eddd9bfea016c
Liam McCartney <liam.mccartney@gmail.com>
2015-01-07 21:28:31 -0500

    changes share link to new domain

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -3,7 +3,7 @@ module FacebookHelper
   def share(args)
     user = FbGraph::User.me(args[:token])
     user.link!(
-      link: "http://cryptic-dusk-7305.herokuapp.com",
+      link: "http://stere-o-gram.herokuapp.com/",
       message: 'I just created a Snippet on Stereogram'
     )
   end
