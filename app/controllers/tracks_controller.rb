f677e4fedf7e0eb28564144fdf85ee18177dd078
Liam McCartney <liam.mccartney@gmail.com>
2015-01-20 22:39:40 -0500

    calls authenticate_user before only tracks#create

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,5 +1,5 @@
 class TracksController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:create]
   def index
     @tracks = Track.all
   end
