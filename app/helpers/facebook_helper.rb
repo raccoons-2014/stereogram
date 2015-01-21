da4f75c10622d31b232ac7e3f6f149eb673027a3
Liam McCartney <liam.mccartney@gmail.com>
2015-01-20 22:42:38 -0500

    changes share link to /snippets instead of specific new snippet

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -3,7 +3,7 @@ module FacebookHelper
   def share(args)
     user = FbGraph::User.me(args[:token])
     user.link!(
-      link: "http://stere-o-gram.herokuapp.com/#{args[:item].class.to_s.downcase}s/#{args[:item].id}",
+      link: "http://stere-o-gram.herokuapp.com/#{args[:item].class.to_s.downcase}s/",
       message: "Check out this #{args[:item].class.to_s} on Stereogram"
     )
   end
